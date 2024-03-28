d4097ee9a058d76a02c1121fcb446fdbb12f0921
AtanasovCode <a_ljupce@yahoo.com>
2024-03-28 14:19:01 +0100

    created tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGradientStart: "hsl(6, 100%, 80%)",
+        primaryGradientEnd: "hsl(335, 100%, 65%)",
+        paleBlue: "hsl(243, 100%, 93%)",
+        grayishBlue: "hsl(229, 7%, 55%)",
+        darkBlue: "hsl(228, 56%, 26%)",
+        veryDarkBlue: "hsl(229, 57%, 11%)",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
